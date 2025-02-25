209eb1e958cbe41fc207255a1039c311e68df031
Sara Sundin <sarasundin@rocketmail.com>
2025-02-25 14:08:01 +0100

    update databases

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #}
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", ""))
+
 }
 
 
