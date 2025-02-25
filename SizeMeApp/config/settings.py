ad79223c5809a1ecbc86a5297da84b2ce3c5e039
Sara Sundin <sarasundin@rocketmail.com>
2025-02-25 08:37:21 +0100

    setup for deployment

--- SizeMeApp/config/settings.py
+++ SizeMeApp/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 DATABASE_URL = config('DATABASE_URL', default='')
 
