be7529856a305665147f34240b96d8fc039763f6
Sara Sundin <sarasundin@rocketmail.com>
2025-02-27 14:08:00 +0100

    reset to prepare for new app accounts

--- config/settings.py
+++ config/settings.py
@@ -140,9 +140,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
