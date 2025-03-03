69205a2a579f84cf50a8633d57f5c5483731f826
Sara Sundin <sarasundin@rocketmail.com>
2025-03-03 15:10:56 +0100

    configure .env settings

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = config("SECRET_KEY")
-DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
