adcd021cefc1b1c931fddcb75235dbc81fdce12b
Sara Sundin <sarasundin@rocketmail.com>
2025-03-03 14:52:53 +0100

    add accounts - user section to admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["email", "name", "is_staff", "is_active"]  # Customize as needed
+    ordering = ["email"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("name", "profile_picture")}),
+        ("Measurements", {"fields": ("chest", "waist", "hips", "shoulders")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "name", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)  # Register the model
