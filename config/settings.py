ce84bd5611de8c5fa9aaeed59ac35a0ece964849
Sara Sundin <sarasundin@rocketmail.com>
2025-03-04 20:47:29 +0100

    debug to false

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
