6e0849fb989ee9ab5fbd8c9f6dc053b520890ce7
Sara Sundin <sarasundin@rocketmail.com>
2025-03-05 08:37:02 +0100

    remove contact section on home and about page

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
