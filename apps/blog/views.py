9ead13cb4159cc6122fd4ef9c4652924c915771e
Sara Sundin <sarasundin@rocketmail.com>
2025-03-13 14:51:20 +0100

    redirect to comment form after editing or deleting comment

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -5,32 +5,20 @@ from django.http import HttpResponseRedirect
 from .models import Post, Comment
 from .forms import CommentForm
 
-# Create your views here.
-
-
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/blog.html"
     paginate_by = 6
 
-
 def post_detail(request, slug):
     """
-    Display an individual :model:`blog.Post`.
-
-    **Context**
-
-    ``post``
-        An instance of :model:`blog.Post`.
-
-    **Template:**
-
-    :template:`blog/post_detail.html`
+    Display an individual post along with comments.
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+
     if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -38,12 +26,11 @@ def post_detail(request, slug):
             comment.author = request.user
             comment.post = post
             comment.save()
-            messages.add_message(
-                request, messages.SUCCESS,
-                'Comment submitted and awaiting approval'
-            )
-    
-    comment_form = CommentForm()
+            messages.success(request, "Comment submitted and awaiting approval.")
+            return HttpResponseRedirect(reverse("post_detail", args=[slug]) + "#message-container")
+
+    else:
+        comment_form = CommentForm()
 
     return render(
         request,
@@ -52,38 +39,40 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
-            "comment_form": comment_form
+            "comment_form": comment_form,
         },
     )
 
-
 def comment_edit(request, slug, comment_id):
     """
-    view to edit comments
+    View to edit comments and redirect based on action:
+    - When clicking "Edit" → Redirect to #commentForm
+    - When updating comment → Redirect to #message-container
     """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
     if request.method == "POST":
-
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comment = get_object_or_404(Comment, pk=comment_id)
         comment_form = CommentForm(data=request.POST, instance=comment)
-
         if comment_form.is_valid() and comment.author == request.user:
             comment = comment_form.save(commit=False)
             comment.post = post
-            comment.approved = False
+            comment.approved = False  # Require re-approval
             comment.save()
-            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+            messages.success(request, "Comment updated successfully!")
+
+            # Redirect to #message-container after updating
+            return HttpResponseRedirect(reverse("post_detail", args=[slug]) + "#message-container")
         else:
-            messages.add_message(request, messages.ERROR,
-                                 'Error updating comment!')
+            messages.error(request, "Error updating comment!")
 
-    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
-
+    # If GET request or form is invalid, redirect back to #commentForm
+    return HttpResponseRedirect(reverse("post_detail", args=[slug]) + "#commentForm")
 
 def comment_delete(request, slug, comment_id):
     """
-    view to delete comment
+    View to delete comment and redirect to message-container.
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
@@ -91,9 +80,8 @@ def comment_delete(request, slug, comment_id):
 
     if comment.author == request.user:
         comment.delete()
-        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+        messages.success(request, "Comment deleted!")
     else:
-        messages.add_message(request, messages.ERROR,
-                             'You can only delete your own comments!')
+        messages.error(request, "You can only delete your own comments!")
 
-    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+    return HttpResponseRedirect(reverse("post_detail", args=[slug]) + "#message-container")
