9a10ae179b68b4b7465676fce5280a1390fa0905
Sara Sundin <sarasundin@rocketmail.com>
2025-03-20 09:01:40 +0100

    test python code for dashboard CI Python Linter

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class DashboardConfig(AppConfig):
+    """
+    Configuration class for the Dashboard app.
+
+    This class registers the 'dashboard' application within Django
+    and defines the default auto field type.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dashboard'
--- dashboard/forms.py
+++ dashboard/forms.py
@@ -2,9 +2,25 @@ from django import forms
 from django.contrib.auth.forms import UserChangeForm
 from accounts.models import CustomUser
 
+
 class CustomUserUpdateForm(UserChangeForm):
+    """
+    A form for updating user profile details.
+
+    This form allows users to edit their profile information,
+    excluding the password field to prevent unintended changes.
+    """
+
     password = None  # Hide password field in the form
 
     class Meta:
         model = CustomUser
-        fields = ["profile_picture", "name", "email", "chest", "waist", "hips", "shoulders"]
+        fields = [
+            "profile_picture",
+            "name",
+            "email",
+            "chest",
+            "waist",
+            "hips",
+            "shoulders",
+        ]
